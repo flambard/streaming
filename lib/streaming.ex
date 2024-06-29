e13977a3c0da192a34b5785165acc2988b09775f
Markus Flambard <markus.flambard@youcal.com>
2024-06-30 00:08:18 +0200

    Generate library app

---
+++ lib/streaming.ex
@@ -0,0 +1,87 @@
+defmodule Streaming do
+  @moduledoc false
+
+  defmacro streaming({:<-, _, [_, _]} = generator, do: block) do
+    quote do
+      streaming [unquote(generator)] do
+        unquote(block)
+      end
+    end
+  end
+
+  defmacro streaming(args, do: block) do
+    {generators_and_filters, options} = Enum.split_while(args, &(not option?(&1)))
+    [bottom_generator | more_generators] = group_filters_with_generators(generators_and_filters)
+
+    inner_block = expand_bottom_generator(bottom_generator, block)
+
+    for generator <- more_generators, reduce: inner_block do
+      block -> expand_generator(generator, block)
+    end
+    |> expand_optional_uniq(options)
+  end
+
+  ###
+  ### Private functions
+  ###
+
+  defp expand_bottom_generator({{:<-, _, [pattern, generator_expression]}, filters}, block) do
+    filtered_expression = expand_filters(filters, pattern, generator_expression)
+
+    quote do
+      unquote(filtered_expression)
+      |> Stream.map(fn unquote(pattern) -> unquote(block) end)
+    end
+  end
+
+  defp expand_generator({{:<-, _, [pattern, generator_expression]}, filters}, block) do
+    filtered_expression = expand_filters(filters, pattern, generator_expression)
+
+    quote do
+      unquote(filtered_expression)
+      |> Stream.flat_map(fn unquote(pattern) -> unquote(block) end)
+    end
+  end
+
+  defp expand_filters(filters, pattern, generator_expression) do
+    for filter <- filters, reduce: generator_expression do
+      expression ->
+        quote do
+          unquote(expression)
+          |> Stream.filter(fn unquote(pattern) -> unquote(filter) end)
+        end
+    end
+  end
+
+  defp expand_optional_uniq(stream, options) do
+    if Keyword.get(options, :uniq) == true do
+      quote do
+        unquote(stream)
+        |> Stream.uniq()
+      end
+    else
+      stream
+    end
+  end
+
+  defp group_filters_with_generators(generators_and_filters) do
+    group_filters_with_generators(generators_and_filters, [])
+  end
+
+  defp group_filters_with_generators([], acc) do
+    acc
+  end
+
+  defp group_filters_with_generators([{:<-, _, [_, _]} = generator | rest], acc) do
+    {filters, more} = Enum.split_while(rest, &(not generator?(&1)))
+    group_filters_with_generators(more, [{generator, filters} | acc])
+  end
+
+  defp generator?(arg) do
+    match?({:<-, _, [_, _]}, arg)
+  end
+
+  defp option?(arg) do
+    match?({_key, _value}, arg)
+  end
+end
---
+++ test/streaming_test.exs
@@ -0,0 +1,61 @@
+defmodule StreamingTest do
+  use ExUnit.Case
+  doctest Streaming
+
+  import Streaming
+
+  test "single infinite generator" do
+    doubles_stream =
+      streaming x <- Stream.from_index(1) do
+        x * 2
+      end
+
+    assert [2, 4, 6, 8] == Enum.take(doubles_stream, 4)
+  end
+
+  test "multiple generators" do
+    all_combinations_stream =
+      streaming [x <- [1, 2], y <- [3, 4], z <- [5, 6]] do
+        {x, y, z}
+      end
+
+    all_combinations_list =
+      for x <- [1, 2], y <- [3, 4], z <- [5, 6] do
+        {x, y, z}
+      end
+
+    assert all_combinations_list == Enum.to_list(all_combinations_stream)
+  end
+
+  test "generator and filter" do
+    even_integers =
+      streaming [x <- [:a, :b, :c], y <- Stream.from_index(1), rem(y, 2) == 0] do
+        {x, y}
+      end
+
+    assert [a: 2, a: 4, a: 6] == Enum.take(even_integers, 3)
+  end
+
+  test "multiple filters" do
+    even_integers =
+      streaming [
+        x <- Stream.from_index(1),
+        rem(x, 2) == 0,
+        y <- [:a, :b, :c],
+        x != y
+      ] do
+        {x, y}
+      end
+
+    assert [{2, :a}, {2, :b}, {2, :c}] == Enum.take(even_integers, 3)
+  end
+
+  test "uniq" do
+    character_stream =
+      streaming [x <- ~c"ABBA", uniq: true] do
+        x
+      end
+
+    assert ~c"AB" = Enum.to_list(character_stream)
+  end
+end
